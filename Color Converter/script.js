d4153a3334b90de0dcf34a21cfaf4e115f43f60d
kanha <kanhapise41@gmail.com>
2024-07-17 22:15:37 +0530

    added hsl to cc

--- Color Converter/script.js
+++ Color Converter/script.js
@@ -163,6 +163,72 @@ class ColorConvertor {
     return res;
   }
 
+  // HEX to RGB AND HSL
+
+  getHexToRgbCode(arr) {
+    let alpha = null;
+    if (arr.length === 4) {
+      alpha = arr[arr.length - 1];
+      alpha = (parseInt(alpha, 16) / 255).toFixed(3);
+      arr = arr.slice(0, 3);
+    }
+    let rgb = arr.map((item) => parseInt(item, 16));
+    return `rgb${alpha ? "a" : ""}(${rgb[0]}, ${rgb[1]}, ${rgb[2]}${
+      alpha ? `, ${alpha}` : ""
+    })`;
+  }
+
+  getHexToHslCode(arr) {
+    let alpha = null;
+    if (arr.length === 4) {
+      alpha = arr[arr.length - 1];
+      alpha = (parseInt(alpha, 16) / 255).toFixed(3);
+      arr = arr.slice(0, 3);
+    }
+
+    let r = parseInt(arr[0], 16);
+    let g = parseInt(arr[1], 16);
+    let b = parseInt(arr[2], 16);
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    let max = Math.max(r, g, b);
+    let min = Math.min(r, g, b);
+
+    let h,
+      s,
+      l = (max + min) / 2;
+
+    if (max === min) h = s = 0;
+    else {
+      let d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+      switch (max) {
+        case r:
+          h = (g - b) / d + (g < b ? 6 : 0);
+          break;
+        case g:
+          h = (b - r) / d + 2;
+          break;
+        case b:
+          h = (r - g) / d + 4;
+          break;
+      }
+      h /= 6;
+    }
+
+    s = Math.round(s * 100);
+    l = Math.round(l * 100);
+    h = Math.round(h * 360);
+
+    return `hsl${alpha ? "a" : ""}(${h}, ${s}%, ${l}% ${
+      alpha ? `, ${alpha}` : ""
+    })`;
+  }
+
 }
 
 const colorConatiner = document.getElementById("container");
