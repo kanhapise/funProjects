1bd7c39a73f0772e3d6ff267d8914ff445e338f8
kanha <kanhapise41@gmail.com>
2024-07-17 16:28:21 +0530

    Added functions for cc

--- Color Converter/script.js
+++ Color Converter/script.js
@@ -42,7 +42,55 @@ class ColorConvertor {
       }
   }
 
- 
+  convert(colorInputValue = null) {
+    let value = colorInputValue ? colorInputValue : this.inputEl.value;
+
+    if (value.trim()) {
+      let colorSplit = this.findColorName(value);
+      if (this.currentColor === "hsl") this.UpdateHexAndRgb(colorSplit);
+      else if (this.currentColor === "hex")
+        this.updateRgbAndHsl(colorSplit, value);
+      else if (this.currentColor === "rgb") this.updateHexAndHsl(colorSplit);
+      else alert("OOPS! Invalid Color");
+    }
+  }
+
+  UpdateHexAndRgb(color, value) {
+    this.hsl = value;
+    this.hex = this.getHslToHexCode(color);
+    this.rgb = this.getHslToRgbCode(color);
+    this.updateDOM(this.hex, this.rgb);
+    this.secondaryColor = "hsl";
+  }
+
+  updateRgbAndHsl(color, value) {
+    this.hex = value;
+    this.rgb = this.getHexToRgbCode(color);
+    this.hsl = this.getHexToHslCode(color);
+    this.updateDOM(this.rgb, this.hsl, this.hex);
+    this.secondaryColor = "hex";
+  }
+  updateHexAndHsl(color, value) {
+    this.rgb = value;
+    this.hex = this.getRgbToHex(color);
+    this.hsl = this.getRgbToHsl(color);
+    this.updateDOM(this.hex, this.hsl);
+    this.secondaryColor = "rgb";
+  }
+
+  updateDOM(c1, c2, hex) {
+    this.color1.innerHTML = c1;
+    this.color2.innerHTML = c2;
+    if (hex && hex.length === 9) hex = hex.slice(0, 7);
+    if (c1.length === 9) c1 = c1.slice(0, 7);
+    this.colorInputEl.value = hex ? hex : c1;
+  }
+
+  updateColor(e) {
+    const value = e.target.value;
+    this.convert(value);
+    this.inputEl.value = value;
+  }
 }
 
 const colorConatiner = document.getElementById("container");
