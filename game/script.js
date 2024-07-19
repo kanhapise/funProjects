322d57c3de292d18d65874be6324ab86008a168a
kanha <kanhapise41@gmail.com>
2024-07-19 21:00:34 +0530

    added fun for game

--- game/script.js
+++ game/script.js
@@ -18,6 +18,78 @@ canvas.height = canvas.width * heightRatio;
 ctx.canvas.width = 800;
 ctx.canvas.height = ctx.canvas.width * heightRatio;
 
+// Elements
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
+
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0,
+};
+
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
+// Create Elements
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = secondaryColor;
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawScore() {
+  ctx.font = '20px "Balsamiq Sans"';
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+function drawBricks() {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? color : "transparent";
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+}
+
 
 // Event Listeners
 document.addEventListener("keydown", keyDown);
