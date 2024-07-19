6ebc44ddea81c2adc6499d7513556048ab705a25
kanha <kanhapise41@gmail.com>
2024-07-19 22:14:50 +0530

    added more fun for  game

--- game/script.js
+++ game/script.js
@@ -90,6 +90,61 @@ function drawBricks() {
   });
 }
 
+function draw() {
+  // clear
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // draw
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawBricks();
+}
+
+// Animate Elements
+function movePaddle() {
+  paddle.x += paddle.dx;
+  if (paddle.x + paddle.w > canvas.width) paddle.x = canvas.width - paddle.w;
+  if (paddle.x < 0) paddle.x = 0;
+}
+
+function moveBall() {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+  // wall collision
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    // right and left
+    ball.dx *= -1;
+  }
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+    // top and bottom
+    ball.dy *= -1;
+  }
+  // paddle
+  if (
+    ball.x - ball.size > paddle.x &&
+    ball.x + ball.size < paddle.x + paddle.w &&
+    ball.y + ball.size > paddle.y
+  ) {
+    ball.dy = -ball.speed;
+  }
+  // bricks
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      if (brick.visible) {
+        if (
+          ball.x - ball.size > brick.x && // left brick side check
+          ball.x + ball.size < brick.x + brick.w && // right brick side check
+          ball.y + ball.size > brick.y && // top brick side check
+          ball.y - ball.size < brick.y + brick.h // bottom brick side check
+        ) {
+          ball.dy *= -1;
+          brick.visible = false;
+          increaseScore();
+        }
+      }
+    });
+  });
+}
 
 // Event Listeners
 document.addEventListener("keydown", keyDown);
