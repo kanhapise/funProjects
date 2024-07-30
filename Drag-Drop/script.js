df80f613d88ceea1172242b8e90ffbe4ba22a769
kanha <kanhapise41@gmail.com>
2024-07-30 17:30:05 +0530

    done js for drag upload

--- Drag-Drop/script.js
+++ Drag-Drop/script.js
@@ -41,3 +41,52 @@ function dropItem(e, currentFile) {
   currentFile.forEach((file) => (file.fileId = getRandomFileId()));
   dropFile(currentFile);
 }
+
+function dropFile(currentFile) {
+  files.push(...currentFile);
+  uploadedFileContEl.innerHTML = null;
+  files.forEach((data) => createFileHolderEl(data));
+}
+
+function createFileHolderEl(file) {
+  const uploadedFileEl = document.createElement("div");
+  uploadedFileEl.classList.add("uploadedFile");
+
+  
+  const fileNameCont = document.createElement("div");
+  fileNameCont.classList.add("fileName");
+
+  const fileName = document.createElement("p");
+  fileNameCont.appendChild(fileName);
+  fileName.innerHTML = file.name; //
+  uploadedFileEl.appendChild(fileNameCont);
+
+  const fileSize = document.createElement("span");
+  fileNameCont.appendChild(fileSize);
+  let sizeInMb =
+    (file.size / (1024 * 1024)).toFixed(2) <= 0
+      ? `${file.size} KB`
+      : `${(file.size / (1024 * 1024)).toFixed(2)} MB`;
+  fileSize.innerHTML = sizeInMb;
+  const closeBtn = document.createElement("div");
+  closeBtn.classList.add("closeBtn");
+  uploadedFileEl.appendChild(closeBtn);
+  closeBtn.innerHTML = `<ion-icon name="close"></ion-icon>`;
+  uploadedFileContEl.prepend(uploadedFileEl);
+  closeBtn.addEventListener("click", (e) => {
+    uploadedFileEl.remove();
+    removeFile(file.fileId);
+    if (!files.length) uploadBtnContEl.classList.remove("content-here");
+  });
+  if (files.length) uploadBtnContEl.classList.add("content-here");
+  dropAreaEl.classList.remove("content-over");
+  dropTextEl.innerHTML = `Drag & Drop your files`;
+}
+
+function getRandomFileId() {
+  return Math.floor(Math.random() * 10000000).toString(16);
+}
+
+function removeFile(fileId) {
+  return (files = files.filter((file) => file.fileId !== fileId));
+}
